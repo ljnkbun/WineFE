c091919c47a03d824dc3cbd395100c62a607e649
linh.hoangtuan <linh.hoangtuan@gits.com.vn>
2024-02-29 17:37:36 +0700

    dsg

--- components/slides/Slides.jsx
+++ components/slides/Slides.jsx
@@ -1,24 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getSlides } from "../../pages/api/slides/imgslide";
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Slides() {
   const sliders = getSlides();
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  const delay = 2000;
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === sliders.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
   return (
-    <div className="slider">
-      <div className="slides">
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {sliders.map((slider, index) => (
+          <div
+            className="slide"
+            key={slider.id}
+          >
+            <Image key={slider.id} className="slide" src={slider.imageUrl} alt={slider.title} width={500} height={400}/>
+          </div>
+        ))}
+      </div>
 
-        {Object.entries(sliders).map((slider, index) => {
-
-          return (
-            <div name={slider[1].title} key={slider[1].id} >
-              <Image key={slider[1].id} src={slider[1].imageUrl} alt={slider[1].title} width={1024} height={300} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
-
-            </div>
-          );
-        })}
-
+      <div className="slideshowDots">
+        {sliders.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
